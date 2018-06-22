c8e5940033bfdb4942feb00f405b4df020e3f1ff
Augustin Cavalier <waddlesplash@gmail.com>
2018-06-21 23:30:02 -0400

    Update VERSION to 2.5rh.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,5 +1,5 @@
 /* Keep JAMVERSYM in sync with VERSION. */
 /* It can be accessed as $(JAMVERSION) in the Jamfile. */
 
-#define VERSION "2.5-haiku-20111222"
-#define JAMVERSYM "JAMVERSION=2.5-haiku-20111222"
+#define VERSION "2.5rh-haiku-20180621"
+#define JAMVERSYM "JAMVERSION=2.5rh-haiku-20180621"
