f425fc556299add0271330b09d0c313946a052e5
Jérôme Duval <jerome.duval@gmail.com>
2018-06-21 23:19:34 -0400

    jam: use posix_spawnp() instead of fork() on linux.
    the gain is about 10% on a 8-job kernel full build.

--- src/execunix.cpp
+++ src/execunix.cpp
@@ -50,9 +50,18 @@
 #include <process.h>
 #endif
 
+#ifdef __linux__
+#define USE_POSIX_SPAWN
+#endif
+
 #ifdef unix
 #include <unistd.h>
 #include <sys/wait.h>
+
+#ifdef USE_POSIX_SPAWN
+extern char **environ;
+#include <spawn.h>
+#endif
 #endif
 
 #ifdef OS_NT
@@ -226,6 +235,12 @@ void execcmd(char* string, void (*func)(void* closure, int status),
 		exit(EXITBAD);
 	}
 #else
+#ifdef USE_POSIX_SPAWN
+	if (posix_spawnp(&pid, argv[0], NULL, NULL, (char * const*)argv, environ) != 0) {
+	    perror("posix_spawnp");
+	    exit(EXITBAD);
+	}
+#else
 #ifdef NO_VFORK
 	if ((pid = fork()) == 0) {
 		execvp(argv[0], argv);
@@ -242,6 +257,7 @@ void execcmd(char* string, void (*func)(void* closure, int status),
 		perror("vfork");
 		exit(EXITBAD);
 	}
+#endif
 #endif
 	/* Save the operation for execwait() to find. */
 
